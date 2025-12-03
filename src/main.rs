d92b42a331cb5076ae6162bc19172fd2478b6eea
DumbAsARoc <timbishop514@gmail.com>
2025-12-03 09:43:21 -0500

    deprecating old ui for rewrite

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@
 mod application_data;
 mod get_endpoints;
 mod main_application;
-mod ui;
+// mod ui;
 
-#[cfg(not(test))]
 use env_logger::{self, Env};
 use main_application::MainApplication;
 
@@ -16,7 +15,6 @@ use main_application::MainApplication;
 ///
 /// Debug contexts will log up through "trace".<br>
 /// Release contexts will log up through "warn".
-#[cfg(not(test))]
 fn initialize_logger() {
     #[cfg(debug_assertions)]
     env_logger::init_from_env(Env::default().default_filter_or("trace"));
@@ -25,35 +23,16 @@ fn initialize_logger() {
     env_logger::init_from_env(Env::default().default_filter_or("warn"));
 }
 
-#[cfg(not(test))]
 #[tokio::main]
 async fn main() -> gtk4::glib::ExitCode {
     initialize_logger();
 
     let handle = tokio::spawn(get_endpoints::run_webserver());
 
-    let app = MainApplication::new();
-    let exitcode = app.run_application();
+    // let app = MainApplication::new();
+    // let exitcode = app.run_application();
 
     handle.abort();
 
     exitcode
-}
-
-// ---------------
-// ---- TESTS ----
-// ---------------
-
-#[cfg(test)]
-mod tests {
-    use crate::MainApplication;
-    use gtk_tester::create_test;
-
-    create_test! {
-        test_if_app_opens,
-        MainApplication,
-        |_win| {
-
-        }
-    }
 }
