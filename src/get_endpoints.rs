bd92615b34b4b326095a63348eec404ce55f8954
DumbAsARoc <timbishop514@gmail.com>
2025-12-02 19:50:19 -0500

    added module file and endpoint overview, along with the packages into cargo.toml

---
+++ src/get_endpoints.rs
@@ -0,0 +1,6 @@
+//! GET Endpoints for overlay applications to use
+//!
+//! API is as follows:
+//!   - /data_json => returns a JSON version of the data in
+//!     the software currently. Used in overlays to get
+//!     data for the webpage to update with
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![windows_subsystem = "windows"]
 
 mod application_data;
+mod get_endpoints;
 mod main_application;
 mod ui;
 
@@ -9,26 +10,19 @@ mod ui;
 use env_logger::{self, Env};
 use main_application::MainApplication;
 
-
 /// Sets up the logger for output, depending
 /// on whether we are in a debug context
 /// or not.
-/// 
+///
 /// Debug contexts will log up through "trace".<br>
 /// Release contexts will log up through "warn".
 #[cfg(not(test))]
 fn initialize_logger() {
     #[cfg(debug_assertions)]
-    env_logger::init_from_env(
-        Env::default()
-            .default_filter_or("trace")
-    );
+    env_logger::init_from_env(Env::default().default_filter_or("trace"));
 
     #[cfg(not(debug_assertions))]
-    env_logger::init_from_env(
-        Env::default()
-            .default_filter_or("warn")  
-    );
+    env_logger::init_from_env(Env::default().default_filter_or("warn"));
 }
 
 #[cfg(not(test))]
@@ -45,10 +39,10 @@ fn main() -> gtk4::glib::ExitCode {
 
 #[cfg(test)]
 mod tests {
-    use gtk_tester::create_test;
     use crate::MainApplication;
+    use gtk_tester::create_test;
 
-    create_test!{
+    create_test! {
         test_if_app_opens,
         MainApplication,
         |_win| {
