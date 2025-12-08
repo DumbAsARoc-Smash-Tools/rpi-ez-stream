cf74f526084466513228b99be40b111002944f1d
Tim Bishop <timbishop514@gmail.com>
2025-12-07 23:31:12 -0500

    added edit callbacks to text input primitive

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,13 @@ slint::include_modules!();
 fn main() -> anyhow::Result<()> {
     let app = MainWindow::new()?;
 
+    app.on_player_1_tag_changed(|new_tag| {
+        println!("Player 1 Tag changed: {new_tag}");
+    });
+
+    app.on_player_2_tag_changed(|new_tag| {
+        println!("Player 2 Tag changed: {new_tag}");
+    });
+
     Ok(app.run()?)
 }
